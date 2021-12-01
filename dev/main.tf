6cb834a76197c8564b34fbcef0321de2020cece2
Sergey Ovsianikov <sg.ovsianikov@gmail.com>
2021-12-01 16:51:17 +0200

    Update outputs

--- dev/main.tf
+++ dev/main.tf
@@ -7,10 +7,9 @@ module "my_vpc" {
   source      = "../modules/vpc"
   vpc_cidr    = "192.168.0.0/16"
   tenancy     = "default"
-  vpc_id      = "${module.my_vpc.vpc_id}"
   subnet_cidr = "192.168.1.0/24"
 }
 
 output "vpc_id" {
-  value = module.my_vpc.vpc_id
+  value = module.my_vpc.id
 }
--- modules/vpc/vars.tf
+++ modules/vpc/vars.tf
@@ -6,8 +6,6 @@ variable "tenancy" {
   default = "dedicated"
 }
 
-variable "vpc_id" {}
-
 variable "subnet_cidr" {
   default = "10.0.1.0/24"
 }
